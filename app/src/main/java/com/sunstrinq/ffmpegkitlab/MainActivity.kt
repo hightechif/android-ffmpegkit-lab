4ad6d7cb4c052ffa2ad1efac0dd1ac5d9e8bd44f
R. Fadhil <ridhanfadhilah@gmail.com>
2026-01-19 15:01:27 +0700

    SETUP: First Commit

---
+++ app/src/main/java/com/sunstrinq/ffmpegkitlab/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sunstrinq.ffmpegkitlab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sunstrinq.ffmpegkitlab.ui.theme.FFmpegKitLabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FFmpegKitLabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FFmpegKitLabTheme {
+        Greeting("Android")
+    }
+}
